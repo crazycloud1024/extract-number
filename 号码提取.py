f9cb786298aa97926c5b9bf21f4dc7ca5ece4300
luoqian67 <originalsvampire67@gmail.com>
2019-01-24 22:39:28 +0800

    '完成提取号码存入excel'

--- 号码提取.py
+++ 号码提取.py
@@ -4,10 +4,12 @@
 # # @File    : 号码提取.py
 # # @Software: PyCharm
 #
+
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from pathlib import Path
-import re
+import re, datetime
+import openpyxl
 
 # regex = re.compile(
 #     '(1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[01356789]\d{2}|4(?:0\d|1[0-2]|9\d))|9[189]\d{2}|6[567]\d{2}|4(?:[14]0\d{3}|[68]\d{4}|[579]\d{2}))\d{6})')
@@ -32,19 +34,32 @@ class Application(tk.Frame):
         self.quit.pack(side="bottom")
 
     def open_file(self):
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+        results = []
+        tar = {'zip', 'rar', '7z'}
+        row = 'ABCDEF'
+        first = ['号码', '脚本执行率', '电话营销语速', '电话营销态度', '电话营销技巧', '营销准确性']
         file_path = filedialog.askopenfilenames()
-        with open('d:/number.txt', 'w+', encoding='utf-8') as fi:
-            for f in file_path:
+        for f in file_path:
+            _, x = f.split('.', 1)
+            if x not in  tar:
                 fd = Path(f).name
                 result = regex.findall(fd)
-                print(result[0])
                 if result:
-                    fi.write((result[0] + "\n"))
-            tk.messagebox.showinfo('提示', '保存成功，请去D盘寻找文件')
+                    results.append(result[0])
+        length = len(results)
+        for i, j in enumerate(row):
+            sheet[str(j) + str(1)] = first[i]
+        for i in range(length):
+            sheet['A' + str(i + 2)] = results[i]
+        workbook.save('D:/upload/{}.xlsx'.format(int(datetime.datetime.now().timestamp())))
+        workbook.close()
+        tk.messagebox.showinfo('提示', '保存成功，请去D盘寻找文件')
 
 
 root = tk.Tk()
-root.title(u"来自你的$$")
+root.title(u"提取号码")
 root.geometry('400x180')
 
 theLabel = tk.Label(root,
